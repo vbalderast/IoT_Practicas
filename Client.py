423c62870107ed0b472b9dc9f4f9c153872bab58
vbalderast <val-balderas@hotmail.com>
2023-02-01 18:21:03 -0800

    Client Server working base

--- Client.py
+++ Client.py
@@ -1,26 +1,19 @@
-#!/usr/bin/env python
-
-#Variables
-host = '192.168.1.98'
-port = 5001
-#Se importa el módulo
 import socket
-#Creación de un objeto socket (lado cliente)
+import time
+
+host = '127.0.0.2'
+port = 5001
+
 obj = socket.socket()
 
-#Conexión con el servidor. Parametros: IP (puede ser del tipo 192.168.1.1 o loc>
 obj.connect((host, port))
-print("Conectado al servidor")
-#Creamos un bucle para retener la conexion
+print("Conectado al servidor") 
 while True:
-    #Instanciamos una entrada de datos para que el cliente pueda enviar mensajes
     mens = input("Mensaje desde Cliente a Servidor >> ")
 
-    #Con el método send, enviamos el mensaje
-    obj.send((mens))
-#Cerramos la instancia del objeto servidor
+    obj.send(mens.encode())
+    time.sleep(1)
+
 obj.close()
 
-#Imprimimos la palabra Adios para cuando se cierre la conexion
 print("Conexión cerrada")
-
--- Server.py
+++ Server.py
@@ -1,30 +1,18 @@
-#!/usr/bin/env python
-#Se importa el módulo
+
 import socket
 
-#instanciamos un objeto para trabajar con el socket
 ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Puerto y servidor que debe escuchar
-ser.bind(("192.168.1.98", 5001))
+ser.bind(("127.0.0.2", 5001))
 
-#Aceptamos conexiones entrantes con el metodo listen. Por parámetro las conexio>
 ser.listen(1)
-#Instanciamos un objeto cli (socket cliente) para recibir datos
-cli, addr = ser.accept()
+(cli, addr)= ser.accept()
 
 while True:
 
-    #Recibimos el mensaje, con el metodo recv recibimos datos. Por parametro la>
-    recibido = cli.recv(1024)
+    recibido = cli.recv(1024).decode()
+    print("Recibo conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1]))
+    print("Cliente: "+recibido)
 
-    #Si se reciben datos nos muestra la IP y el mensaje recibido
-    print("Recibo conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[>
-
-    #Devolvemos el mensaje al cliente
-    cli.send(recibido)
-
-
-#Cerramos la instancia del socket cliente y servidor
 cli.close()
 ser.close()
 
